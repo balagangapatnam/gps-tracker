4543bed8fedb550844955922c9ef97c52c4c54c2
Bala <bala.gangapatnam@gmail.com>
2020-08-21 00:25:31 +0100

    First Commit.

---
+++ src/GPSTracker/WebApp/ClientApp/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Route } from "react-router";
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return <h1>Welcome to GPS tracker!!</h1>;
+  }
+}
