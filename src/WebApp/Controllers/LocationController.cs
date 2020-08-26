65bdb9a649fc8b26dbda960ba47afac744dc1a51
Bala <bala.gangapatnam@gmail.com>
2020-08-26 18:04:44 +0100

    Added response types

--- src/WebApp/Controllers/LocationController.cs
+++ src/WebApp/Controllers/LocationController.cs
@@ -6,6 +6,7 @@ using LocationService.Code.GetUsersWithInAnArea;
 using LocationService.Code.PutUserLocation;
 using LocationService.Data;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -21,14 +22,17 @@ namespace WebApp.Controllers
         public LocationController(IMediator mediator) => _mediator = mediator;
 
         [HttpGet("/users/location")]
+        [ProducesResponseType(typeof(IEnumerable<User>), StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<User>>> GetLocationForAllUsers() =>
             this.Ok(await this._mediator.Send(new GetAllUsersLocationCommand()));
 
         [HttpGet("/users/{userId}/location")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         public async Task<ActionResult<User>> GetLocation(int userId) =>
             this.Ok(await this._mediator.Send(new GetUserLocationCommand(userId)));
 
         [HttpGet("/users/{userId}/location/history")]
+        [ProducesResponseType(typeof(UserHistory), StatusCodes.Status200OK)]
         public async Task<ActionResult<UserHistory>> GetLocationHistory(int userId) =>
             this.Ok(await this._mediator.Send(new GetUserLocationHistoryCommand(userId)));
 
@@ -36,15 +40,12 @@ namespace WebApp.Controllers
         public async Task Put(int userId, [FromBody] Location location) =>
             await this._mediator.Send(new PutUserLocationCommand(userId, location));
 
-
         [HttpGet("/users/location/area")]
         public async Task<ActionResult<IEnumerable<User>>> GetUsersWithInArea([FromQuery] Area area) =>
             this.Ok(await this._mediator.Send(new GetUsersWithInAnAreaCommand(area)));
 
         [HttpPost("/users/{name}/location")]
-        public async Task<ActionResult<IEnumerable<User>>> CreateUserRecord(string name, [FromBody] Location location)
-        {
-            return this.Ok(await this._mediator.Send(new CreateUserCommand(name, location)));
-        }
+        public async Task<ActionResult<User>> CreateUserRecord(string name, [FromBody] Location location) =>
+            await this._mediator.Send(new CreateUserCommand(name, location));
     }
 }
